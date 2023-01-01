0f5ac728032a492202c051f6fdf3f9fdc86d2daf
Arda Iskender <iskenderarda@gmail.com>
2023-01-01 15:54:54 +0300

    Easy: Randomize Obstacles
    - Added 3 obstacles.
    - Spawn manager now instantiates between 4 obstacles randomly

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    public GameObject obstaclePrefab;
+    public GameObject []obstaclePrefab;
     private Vector3 spawnPos = new Vector3(25, 0, 0);
 
     private float startDelay = 2f;
@@ -27,7 +27,8 @@ public class SpawnManager : MonoBehaviour
     {
         if (playerControllerScript.gameOver == false)
         {
-            Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+            int prefab = Random.Range(0, obstaclePrefab.Length);
+            Instantiate(obstaclePrefab[prefab], spawnPos, obstaclePrefab[prefab].transform.rotation);
         }
         
     }
